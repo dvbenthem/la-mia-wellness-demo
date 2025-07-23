d91b0c0dca9cf1e7a3cd36843bc78bd04ac631c6
dvbenthem <46499120+dvbenthem@users.noreply.github.com>
2025-07-23 17:49:27 +0200

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let mobileMenuButton = document.querySelector(".mobile-menu");
+let mobileMenu = document.querySelector(".menu");
+
+
+mobileMenuButton.addEventListener("click", () => {
+
+  mobileMenu.classList.toggle("active");
+
+});
